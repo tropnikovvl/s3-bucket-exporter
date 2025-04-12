e6743dddb9f977c0a5c8e4a9136208e6974b0147
Vladislav Tropnikov <vladislav.tropnikov@genestack.com>
2025-04-12 21:58:07 +0200

    fix

--- auth/aws.go
+++ auth/aws.go
@@ -79,7 +79,7 @@ func (a *AWSAuth) GetConfig(ctx context.Context) (aws.Config, error) {
 
 	if a.cfg.SkipTLSVerify {
 		customTransport := http.DefaultTransport.(*http.Transport).Clone()
-		customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+		customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: a.cfg.SkipTLSVerify}
 		options = append(options, config.WithHTTPClient(&http.Client{
 			Transport: customTransport,
 		}))
